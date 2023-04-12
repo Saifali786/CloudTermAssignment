b48a53d623f248896b18a61b90e27d1e3682a101
Saifali786 <41464847+Saifali786@users.noreply.github.com>
2023-04-12 15:09:44 -0300

    Update SentimentAnalysis.js

--- src/SentimentAnalysis.js
+++ src/SentimentAnalysis.js
@@ -22,7 +22,7 @@ export const SentimentAnalysis = () => {
       objectKey: "userdata.txt",
     };
     const result = await fetch(
-      "https://kzcwbc779e.execute-api.us-east-1.amazonaws.com/deploy/sentiment",
+      "https://n4xo4nyqj7.execute-api.us-east-1.amazonaws.com/deploy/sentiment",
       {
         method: "POST",
         headers: {
@@ -50,7 +50,7 @@ export const SentimentAnalysis = () => {
       message: data,
     };
     const response = await fetch(
-      "https://kzcwbc779e.execute-api.us-east-1.amazonaws.com/deploy/fetch-data",
+      "https://n4xo4nyqj7.execute-api.us-east-1.amazonaws.com/deploy/fetch-data",
       {
         method: "POST",
         headers: {
